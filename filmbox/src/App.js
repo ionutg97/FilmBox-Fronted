8b53a161753f18b6cea45464f320fff85aa7dd46
ionut galan <ionut_and97@yahoo.com>
2019-11-01 00:44:29 +0200

    Create video component and rendering video

---
+++ filmbox/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import List from './modules/dashbordFilm/container/List';
+
+function App() {
+  return (
+    <div className="App">
+      <List>
+        
+      </List>
+    </div>
+  );
+}
+
+export default App;
